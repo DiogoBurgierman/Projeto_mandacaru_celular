95b763524397e990a29fcf3a1ee7a7417733e8f9
DiogoBurgierman <124000765+DiogoBurgierman@users.noreply.github.com>
2023-02-08 20:55:51 -0300

    movimentação é agora através dos botoes de comando (o jogo esta gerando um erro que vou corrigir ainda)
    movimentação é agora através dos botoes de comando (o jogo esta gerando um erro que vou corrigir ainda)

--- Scripts/Cena principal.gd
+++ Scripts/Cena principal.gd
@@ -4,10 +4,10 @@ var next = false
 
 
 func _ready():
-	var control_screen = load("res://Screens/teladeseleção.tscn").instance()
+	var control_screen = load("res://Screens/AbaDeSelecao.tscn").instance()
 	get_tree().current_scene.add_child(control_screen)
 
 
 func _on_Area2D_body_entered(body):
-	$KinematicBody2D.block = ''
+	#$KinematicBody2D.block = ''
 	next = true
--- Scripts/KinematicBody2D.gd
+++ Scripts/KinematicBody2D.gd
@@ -1,36 +1,49 @@
 extends KinematicBody2D
 
-var block = ''
-
 var velocidade = 400
+
+var i = 0
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
+	
+	var x=get_parent().get_node("AbaDeSelecao/Comandos")
+	
 	var movimento = Vector2()
 	
-	if Input.is_action_pressed("ui_left"):
-		block = 'left'
-
-	elif Input.is_action_pressed("ui_right"):
-		block = 'right'
-
-	elif Input.is_action_pressed("ui_down"):
-		block = 'down'
+	if x.iniciou==true:
 		
-	elif Input.is_action_pressed("ui_up"):
-		block = 'up'
 		
-	if block == 'left':
-		movimento.x -= velocidade
-	elif block == 'right': 
-		movimento.x += velocidade
-	elif block == 'down':
-		movimento.y += velocidade
-	elif block == 'up':
-		movimento.y -= velocidade			
-	
-	movimento = movimento.normalized()
-	
-	move_and_slide(movimento*velocidade)
+		#if Input.is_action_pressed("ui_left"):
+			#block = 'esquerda'
+
+		#elif Input.is_action_pressed("ui_right"):
+			#block = 'direita'
+
+		#elif Input.is_action_pressed("ui_down"):
+			#block = 'baixo'
+			
+		#elif Input.is_action_pressed("ui_up"):
+			#block = 'cima'
+			
+		if x.array[i] == "esquerda":
+			movimento.x -= velocidade
+		elif x.array[i] == "direita": 
+			movimento.x += velocidade
+		elif x.array[i] == "baixo":
+			movimento.y += velocidade
+		elif x.array[i] == "cima":
+			movimento.y -= velocidade			
+			
+		movimento = movimento.normalized()
+		
+		var y =move_and_slide(movimento*velocidade)
+		
+		if y.x<10.0 and y.y<10.0:
+			print(i)
+			i = i + 1
+			
+		
+		
